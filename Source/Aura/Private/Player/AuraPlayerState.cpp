999928480fef17a8217805f3008de48d0c03057b
Oleksandr Vashchenko <vashchenko.casha@gmail.com>
2025-01-08 19:09:19 +0200

    v3.2

--- Source/Aura/Private/Player/AuraPlayerState.cpp
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -3,7 +3,21 @@
 
 #include "Player/AuraPlayerState.h"
 
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "AblitySystem/AuraAbilitySystemComponent.h"
+
 AAuraPlayerState::AAuraPlayerState()
 {
+	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+
+
+	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
+	
 	NetUpdateFrequency = 100.f;
+}
+
+UAbilitySystemComponent* AAuraPlayerState::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
 }
