ef45ced7fd1823b117fb0c9e53a11e37dadea87b
Oleksandr Vashchenko <vashchenko.casha@gmail.com>
2025-02-10 03:01:13 +0200

    v9.6

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// Copyright Vashchenko
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+	
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+	
+}
