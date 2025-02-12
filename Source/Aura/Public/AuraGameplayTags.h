3aa4ecf11c490e5e7e32883abe652e5d6e4116df
Oleksandr Vashchenko <vashchenko.casha@gmail.com>
2025-02-12 23:28:25 +0200

    v10.2

--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -33,8 +33,15 @@ public:
  FGameplayTag Attributes_Secondary_MaxHealth;
  FGameplayTag Attributes_Secondary_MaxMana;
 
- FGameplayTag Attributes_Vital_Health;
- FGameplayTag Attributes_Vital_Mana;
+ /*FGameplayTag Attributes_Vital_Health;
+ FGameplayTag Attributes_Vital_Mana;*/
+
+ FGameplayTag InputTag_LMB;
+ FGameplayTag InputTag_RMB;
+ FGameplayTag InputTag_1;
+ FGameplayTag InputTag_2;
+ FGameplayTag InputTag_3;
+ FGameplayTag InputTag_4;
 
 protected:
 
